6c6b1b4ddfa18858645068dfd03bf81d5db87c60
svaddemani <svaddemani@innominds.com>
2023-07-11 08:35:04 +0530

    sd

--- server/index.js
+++ server/index.js
@@ -51,11 +51,11 @@ app.get('/sessions/:id', (req, res, next) => {
             .replace('__META_OG_TITLE__', postId)
             .replace('__META_OG_DESCRIPTION__', `This is description of session ${postId}`)
             .replace('__META_DESCRIPTION__', `This is description of session ${postId}`)
-            .replace('__META_OG_IMAGE__', postId <= 4 ? imgs[postId] : "post.thumbnail")
+            .replace('__META_OG_IMAGE__', postId <= 4 ? imgs[postId] : post.thumbnail)
 
             .replace("__META_TWITTER_TITLE__", postId)
             .replace("__META_TWITTER_DESCRIPTION__",  `This is description of session ${postId}`)
-            .replace("__META_TWITTER_IMAGE__", postId <= 4 ? imgs[postId] : "post.thumbnail")
+            .replace("__META_TWITTER_IMAGE__", postId <= 4 ? imgs[postId] : post.thumbnail)
         return res.send(htmlData);
     });
 });
