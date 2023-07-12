705819983ac56d48d03ff77d0cfb4df7c0ff8f39
svaddemani <svaddemani@innominds.com>
2023-07-12 15:38:26 +0530

    dfdfs

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const indexPath = path.resolve(__dirname, '..', 'build', 'index.html');
 
 const imgs = [
     "https://fastly.picsum.photos/id/17/2500/1667.jpg?hmac=HD-JrnNUZjFiP2UZQvWcKrgLoC_pc_ouUSWv8kHsJJY",
-    `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=1200x628&maptype=roadmap&format=png&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=${process.env.API_KEY}`
+    `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=1200x675&maptype=roadmap&format=png&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=${process.env.API_KEY}`
 ];
 
 // static resources should just be served as they are
